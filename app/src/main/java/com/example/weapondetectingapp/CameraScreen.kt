83346af621f85ed4d3a4dc1c80c69848fdfbad1c
justrn <77417694+justrn@users.noreply.github.com>
2021-01-16 16:02:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weapondetectingapp/CameraScreen.kt
@@ -0,0 +1,11 @@
+package com.example.weapondetectingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_screen)
+    }
+}
